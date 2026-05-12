6870c301b77c35b33b6d91445b7ed6e87f9469a9
RANJITH A <ranjithannadurai7@gmail.com>
2026-05-12 16:58:00 +0530

    Configure backend for Vercel deployment

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+  if (mongoose.connections[0].readyState) return;
+
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
   } catch (error) {
     console.error(error);
-    process.exit(1);
   }
 };
 
